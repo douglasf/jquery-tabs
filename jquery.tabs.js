de301d17f033007e9b339853098f72abd93a0a95
Douglas Frisk <douglasfrisk@gmail.com>
2011-11-23 03:25:02 +0100

    Changed tabs to soft tabs

--- jquery.tabs.js
+++ jquery.tabs.js
@@ -26,34 +26,34 @@
 
     TabsPlugin.prototype.init = function () {
 
-		var _href,
-			_thisElem = this.element;
-		
-		$($(this.element).siblings("div.tab")).hide();
-		
-		if(this.options._hash && $(this.element).siblings("div.tab").parent().has(this.options._hash).length == 1) {
-			$(this.element).parentsUntil(".tab").parent().show();
-			$("." + $(this.element).parentsUntil(".tab").parent().attr("id")).parent().addClass("selected");
-			$(this.element.children).children(this.options._hash.replace("#",".")).parent().addClass("selected");
-			$(this.options._hash).show();
-		} else {
-			var _this = $(this.element.children).children(".default");
-			_this.parent().addClass("selected");
-			$(_this[0].hash).show();
-		}
-		
-		$(this.element.children).each(function() {
-			$(this.firstChild).bind('click', function(event) {
-				event.preventDefault();
-				$(this).parent().siblings().removeClass("selected");
-				$(this).parent().addClass("selected");
-				window.location.hash = this.hash;
-				_href = $(this).attr("href");
-				$(_thisElem).siblings("div.tab").hide();
-		    	$(_href).show();
-			});
-		});
-		
+        var _href,
+            _thisElem = this.element;
+        
+        $($(this.element).siblings("div.tab")).hide();
+        
+        if(this.options._hash && $(this.element).siblings("div.tab").parent().has(this.options._hash).length == 1) {
+            $(this.element).parentsUntil(".tab").parent().show();
+            $("." + $(this.element).parentsUntil(".tab").parent().attr("id")).parent().addClass("selected");
+            $(this.element.children).children(this.options._hash.replace("#",".")).parent().addClass("selected");
+            $(this.options._hash).show();
+        } else {
+            var _this = $(this.element.children).children(".default");
+            _this.parent().addClass("selected");
+            $(_this[0].hash).show();
+        }
+        
+        $(this.element.children).each(function() {
+            $(this.firstChild).bind('click', function(event) {
+                event.preventDefault();
+                $(this).parent().siblings().removeClass("selected");
+                $(this).parent().addClass("selected");
+                window.location.hash = this.hash;
+                _href = $(this).attr("href");
+                $(_thisElem).siblings("div.tab").hide();
+                $(_href).show();
+            });
+        });
+        
     };
 
     $.fn[pluginName] = function ( options ) {
